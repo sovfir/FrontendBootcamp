5466bc34e90618cd9b6db41cc60800cb00b1c0ba
Sergei Firsov <85520080+sovfir@users.noreply.github.com>
2023-10-04 20:55:45 +0300

    Add files via upload

---
+++ day03/3/calc/script.js
@@ -0,0 +1,33 @@
+const buttons = document.querySelectorAll("button");
+
+const screenDisplay = document.querySelector(".screen");
+
+let calculation = [];
+let clearCalc = [];
+
+function calculate(button) {
+  const value = button.textContent;
+  if (value === "C") {
+    calculation = [];
+    clearCalc = [];
+    screenDisplay.textContent = ".";
+  } else if (value === "=") {
+    let answer = eval(clearCalc);
+    screenDisplay.textContent = answer;
+    clearCalc.push(answer);
+  } else {
+    if (clearCalc.length <= 9) {
+      calculation.push(value);
+      clearCalc = calculation.join("");
+      screenDisplay.textContent = clearCalc;
+    } else {
+      screenDisplay.textContent = ".";
+      calculation = [];
+      clearCalc = [];
+    }
+  }
+}
+
+buttons.forEach((button) =>
+  button.addEventListener("click", () => calculate(button))
+);
